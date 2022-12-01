8d35a2793e0c23adbda2721806bb82982545a708
yuvraj8565 <42003167+yuvraj8565@users.noreply.github.com>
2022-12-01 11:04:40 +0530

    Deployment script
    updated the deployment script

--- task2/app_db_deploy.sh
+++ task2/app_db_deploy.sh
@@ -2,4 +2,5 @@
 ## This is a simple bash script for creating the APP and DB deployments
 ## Clone repo from Public Git
 git clone https://github.com/yuvraj8565/awsyuvarajasses.git
-
+kubectl apply -f awsyuvarajasses/task2/nginxdeploy
+kubectl apply -f awsyuvarajasses/task2/dbdeploy
